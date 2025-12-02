05e8a3b01f75bc986bfc202231024f4581c35408
yiselorena0808 <yiselorenasalzar@gmail.com>
2025-12-02 15:32:50 -0500

    cambios

--- Fast_api/app/ejemplo.py
+++
@@ -1,3 +0,0 @@
-from your_siamese_module import load_pairs, train_model  # según tu código Siamese
-X, y = load_pairs("dataset_example")
-train_model(X, y)
--- Fast_api/app/scripts/matcher/train_verify_net.py
+++
@@ -1,4 +0,0 @@
-# NOTE: move file to root folder to enable training locally
-from src.fingerflow.matcher.VerifyNet import verify_net_train
-
-verify_net_train.train()
